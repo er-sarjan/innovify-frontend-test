4b3609f795065c058fd5a8340425f1f1bc10faf3
Sarjan Desai <er.sarjan686@gmail.com>
2017-11-03 22:14:20 +0530

    Use IIFE for gulp changes and prevent polluting global variable :sparkles: :hammer:

--- src/app/app.js
+++ src/app/app.js
@@ -1,45 +1,48 @@
-const app = angular.module('app', ['ui.router', 'ui.bootstrap',
-	'pascalprecht.translate']);
+(function() {
+	const app = angular.module('app', ['ui.router', 'ui.bootstrap',
+		'pascalprecht.translate']);
 
-app.constant('config', {
-    apiUrl: '../api/'
-});
-
-app.config(HttpProvider);
-
-app.config(translateConfig);
-
-/* @ngInject */
-function translateConfig($translateProvider) {
-
-	$translateProvider.useSanitizeValueStrategy('escapeParameters');
-
-	$translateProvider.useMissingTranslationHandlerLog();
-
-	// TODO: Use file loader instead of static object
-	$translateProvider.translations('en', {
-		HEADING: 'Hello, world!<br>Welcome to home page',
-		BUTTON_LANG_EN: 'English',
-		BUTTON_LANG_DE: 'German'
+	app.constant('config', {
+		apiUrl: '../api/'
 	});
 
-	$translateProvider.translations('de', {
-		HEADING: 'Hallo Welt!<br>Willkommen auf der Startseite',
-		BUTTON_LANG_EN: 'Englisch',
-		BUTTON_LANG_DE: 'Deutsch'
-	});
+	app.config(HttpProvider);
+
+	app.config(translateConfig);
+
+	/* @ngInject */
+	function translateConfig($translateProvider) {
+
+		$translateProvider.useSanitizeValueStrategy('escapeParameters');
+
+		$translateProvider.useMissingTranslationHandlerLog();
+
+		// TODO: Use file loader instead of static object
+		$translateProvider.translations('en', {
+			HEADING: 'Hello, world!<br>Welcome to home page',
+			BUTTON_LANG_EN: 'English',
+			BUTTON_LANG_DE: 'German'
+		});
+
+		$translateProvider.translations('de', {
+			HEADING: 'Hallo Welt!<br>Willkommen auf der Startseite',
+			BUTTON_LANG_EN: 'Englisch',
+			BUTTON_LANG_DE: 'Deutsch'
+		});
 
 
-	$translateProvider.preferredLanguage('en');
-	$translateProvider.fallbackLanguage('en');
-}
+		$translateProvider.preferredLanguage('en');
+		$translateProvider.fallbackLanguage('en');
+	}
 
 
-/* @ngInject */
-function HttpProvider($httpProvider) {
-	$httpProvider.interceptors.push(['$injector',
-		function($injector) {
-			return $injector.get('httpInterceptor').getInterceptor();
-		}
-	]);
-}
+	/* @ngInject */
+	function HttpProvider($httpProvider) {
+		$httpProvider.interceptors.push(['$injector',
+			function($injector) {
+				return $injector.get('httpInterceptor').getInterceptor();
+			}
+		]);
+	}
+
+})();
--- src/app/services/itemsService.js
+++ src/app/services/itemsService.js
@@ -1,31 +1,32 @@
-/**
- * Provides access to the JSON endpoint which contains the data about the items.
- */
-class ItemsService {
+(function() {
+	// TODO: This service is not removed for showing 404 alert using interceptor which shows console log for not found
+	class ItemsService {
 
-    /*@ngInject*/
-    constructor($http, config) {
-        this.$http = $http;
-        this.config = config;
-    }
+		/*@ngInject*/
+		constructor($http, config) {
+			this.$http = $http;
+			this.config = config;
+		}
 
-    getItems() {
-        var apiUrl = this.config.apiUrl;
+		getItems() {
+			const apiUrl = this.config.apiUrl;
 
-        return this.$http
-            .get(apiUrl + 'items')
-            .then((result) => {
-                // prepend the API url to the images
-                return result.data.map((item) => {
+			return this.$http
+					.get(apiUrl + 'items')
+					.then((result) => {
+						// prepend the API url to the images
+						return result.data.map((item) => {
 
-                    item.image = apiUrl + item.image;
-                    item.thumb = apiUrl + item.thumb;
-                    return item;
+							item.image = apiUrl + item.image;
+							item.thumb = apiUrl + item.thumb;
+							return item;
 
-                });
-            });
-    }
+						});
+					});
+		}
 
-}
+	}
 
-register('app').service('itemsService', ItemsService);
+	register('app').service('itemsService', ItemsService);
+
+})();
